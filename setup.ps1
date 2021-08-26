8234a455f82292ecd1d8cc78da06fc6a1dc2d1d7
Jan Bielak <contact@janbielak.com>
2021-08-26 21:35:09 +0200

    Fix check for admin rights
    Signed-off-by: Jan Bielak <contact@janbielak.com>

--- setup.ps1
+++ setup.ps1
@@ -3,10 +3,10 @@ param(
     [System.Boolean]$ExcludeFromDefender = $false
 )
 
-$ScriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 if ($ExcludeFromDefender) {
-    $RunningAsAdmin = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    $RunningAsAdmin = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
     if (-not $RunningAsAdmin) {
         Write-Host "To exclude the folder from Windows Defender, this script requires administrator privilages"
         Exit 1
